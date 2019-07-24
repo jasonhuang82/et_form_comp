650109f0e68f7af10af39d0ede48b02d0d9f4b51
jason <jason.huang@ehsn.coim.tw>
2019-07-24 16:21:29 +0800

    move App.js to containers

---
+++ src/containers/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ul className="row">
+        <li className="col-12 col-sm-6 col-md-4"></li>
+        <li className="col-12 col-sm-6 col-md-4"></li>
+        <li className="col-12 col-sm-6 col-md-4"></li>
+        <li className="col-12 col-sm-6 col-md-4"></li>
+        <li className="col-12 col-sm-6 col-md-4"></li>
+        <li className="col-12 col-sm-6 col-md-4"></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
