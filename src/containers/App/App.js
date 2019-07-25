0dab884ca11462903e052b5dd6107e737048cde3
jason <jason.huang@ehsn.coim.tw>
2019-07-25 12:15:55 +0800

    create react-router structure

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
 import './App.scss';
+
+// pages
+import LoginForm from '../../pages/LoginForm/loginForm';
+import PurchaseForm from '../../pages/PurchaseForm/purchaseForm';
+
+// components
+import Header from '../../components/Header/header';
 
 const App = () => {
   return (
-    <div className="App">
-      <ul className="row">
-        <li className="col-12 col-sm-6 col-md-4"></li>
-        <li className="col-12 col-sm-6 col-md-4"></li>
-        <li className="col-12 col-sm-6 col-md-4"></li>
-        <li className="col-12 col-sm-6 col-md-4"></li>
-        <li className="col-12 col-sm-6 col-md-4"></li>
-        <li className="col-12 col-sm-6 col-md-4"></li>
-      </ul>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <Header />
+        <main className="App_content">
+          <Switch>
+            <Route exact path="/" component={LoginForm} />
+            <Route path="/purchase" component={PurchaseForm} />
+            <Route path="*" render={props => <div>404</div>}/>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
