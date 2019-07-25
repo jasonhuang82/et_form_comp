6739fb64c7449e2a8fa0c1716a4ba9e9b14f46db
jason <jason.huang@ehsn.coim.tw>
2019-07-25 12:16:37 +0800

    create LoginForm page

---
+++ src/pages/LoginForm/loginForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+export default class LoginForm extends Component {
+    render() {
+        return (
+        <div className="LoginForm w-80p h-80vh pos-ab-center bg-white">
+            LoginForm
+        </div>
+        )
+    }
+}
