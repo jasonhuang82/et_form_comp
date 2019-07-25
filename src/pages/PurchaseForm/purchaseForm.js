eace0e613d8573023ef45aed96bdafaae17c5b5b
jason <jason.huang@ehsn.coim.tw>
2019-07-25 12:16:27 +0800

    create PurchaseForm page

---
+++ src/pages/PurchaseForm/purchaseForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+export default class PurchaseForm extends Component {
+    render() {
+        return (
+            <div className="PurchaseForm w-80p h-80vh pos-ab-center bg-white">
+                PurchaseForm
+            </div>
+        )
+    }
+}
