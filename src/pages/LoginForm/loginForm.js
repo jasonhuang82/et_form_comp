d4ce2d4f9034cd4e774c6e342850452e1ea57e6a
jason <jason.huang@ehsn.coim.tw>
2019-07-29 15:36:59 +0800

    extract  form-control to component

--- src/pages/LoginForm/loginForm.js
+++ src/pages/LoginForm/loginForm.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './style.scss';
 
+import FormControl from 'components/FormControls/FormControl/formControl';
+import Input from 'components/FormControls/Input/input';
+import Select from 'components/FormControls/Select/select';
+
 export default class LoginForm extends Component {
     render() {
         return (
@@ -11,73 +15,100 @@ export default class LoginForm extends Component {
                 </div>
 
                 {/* 手機 */}
-                <div className="form-control">
-                    <label htmlFor="">您的手機號碼</label>
+                <FormControl label="您的手機號碼" errorMessage="最少 9 個字">
                     <div className="form-control-combine">
-                        <div className="form-control-combine-select w-40p">
-                            <select>
-                                <option value="86">中國 +86</option>
-                                <option value="852">香港 +852</option>
-                                <option value="853">澳門 +853</option>
-                                <option value="886">台灣 +886</option>
-                            </select>
-                        </div>
-                        <div className="form-control-combine-input form-control-input w-60p">
-                            <input
-                                type="text"
-                                // value="0"
-                                onChange={() => {}}
-                                placeholder="請輸入您的手機號碼"
-                            />
-                        </div>
+                        <Select
+                            wrapClass="form-control-combine-select w-40p d-i-b vertical-align-top"
+                            options={[
+                                {
+                                    name: '中國 +86',
+                                    value: '86',
+                                },
+                                {
+                                    name: '香港 +852',
+                                    value: '852',
+                                },
+                                {
+                                    name: '澳門 +853',
+                                    value: '853',
+                                },
+                                {
+                                    name: '台灣 +886',
+                                    value: '886',
+                                },
+                            ]}
+                        />
+                        <Input wrapClass="form-control-combine-input w-60p  d-i-b vertical-align-top"/>
                     </div>
-                </div>
+                </FormControl>
 
                 {/* 姓名 */}
-                <div className="form-control">
-                    <label htmlFor="">會員姓名</label>
-                    <div className="form-control-input">
-                        <input
-                            type="text"
-                            // value="0"
-                            onChange={() => {}}
-                            placeholder="請輸入真實中文姓名"
-                        />
-                    </div>
-                </div>
+                <FormControl label="會員姓名" errorMessage="">
+                    <Input
+                        type="text"
+                        onChange={() => {}}
+                        placeholder="請輸入真實中文姓名"
+                    />
+                </FormControl>
 
                 {/* 密碼 */}
 
+                <FormControl label="密碼" errorMessage="">
+                    <Input
+                        type="password"
+                        onChange={() => {}}
+                        placeholder="請輸入密碼"
+                    />
+                </FormControl>
                 {/* 確認密碼 */}
 
+                <FormControl label="確認密碼" errorMessage="">
+                    <Input
+                        type="password"
+                        onChange={() => {}}
+                        placeholder="請輸入密碼"
+                    />
+                </FormControl>
                 {/* Email */}
 
+                <FormControl label="Email" errorMessage="">
+                    <Input
+                        type="email"
+                        onChange={() => {}}
+                        placeholder="請輸入 Email"
+                    />
+                </FormControl>
                 {/* 手機驗證碼 */}
-                <div className="form-control">
-                    <label htmlFor="">手機驗證碼</label>
-                    <div className="form-control-input">
-                        <input
-                            type="text"
-                            // value="0"
-                            onChange={() => {}}
-                            placeholder="請輸入真實中文姓名"
-                        />
-                        <button className="LoginForm-phone-btn btn d-i-b w-30p pos-ab t-50p r-5 translate-y-50p">
-                            取得手機驗證碼
-                        </button>
-                    </div>
-                </div>
+                <FormControl label="手機驗證碼" errorMessage="">
+                    <Input
+                        type="email"
+                        onChange={() => {}}
+                        placeholder="請輸入驗證碼"
+                        style={{
+                            paddingRight: '32%',
+                        }}
+                        appendElementRender={() => {
+                            return (
+                                <button
+                                    className="LoginForm-phone-btn btn d-i-b w-30p pos-ab t-50p r-5 translate-y-50p"
+                                >
+                                    取得手機驗證碼
+                                </button>
+                            );
+                        }}
+                    />
+                </FormControl>
                 {/* Submit */}
                 <button
-                    className="LoginForm-submit btn-disable btn btn-submit m-b-sm btn-border"
+                    className="LoginForm-submit btn btn-submit m-b-sm btn-border"
                     disabled
                 >
                     確定註冊
                 </button>
                 {/* Agree */}
-                <div className="form-control">
+                <div className="fz-sm">
                     <label>
-                        <input type="checkbox"/>
+                        <input type="checkbox" className="d-i-b vertical-align-middle"/>
                         <span>我願意接收最新優惠通知</span>
                         <a className="n-blue--link" href="/HelpCenter/MarketInfo" title="行銷資訊發送" target="_blank">&lt;行銷資訊發送&gt;</a>
                     </label>
