b737374c7f989976d51cbf4d33345f0c0d05c551
jason <jason.huang@ehsn.coim.tw>
2019-07-30 17:12:18 +0800

    Add address field

--- src/pages/LoginForm/loginForm.js
+++ src/pages/LoginForm/loginForm.js
@@ -12,20 +12,27 @@ class LoginForm extends Component {
         super(props)
         this.countryOptions = [
             {
-                name: '中國 +86',
-                value: '86',
-            },
-            {
-                name: '香港 +852',
-                value: '852',
-            },
-            {
-                name: '澳門 +853',
-                value: '853',
-            },
-            {
                 name: '台灣 +886',
-                value: '886',
+                value: 886,
+            },
+            {
+                name: '中國 +86',
+                value: 86,
+            },
+        ];
+
+        this.addressOptions = [
+            {
+                name: '請選擇區域',
+                value: '',
+            },
+            {
+                name: '台北市中山區',
+                value: 100,
+            },
+            {
+                name: '台北市內湖區',
+                value: 120,
             },
         ];
     }
@@ -52,155 +59,191 @@ class LoginForm extends Component {
             values,
             errors,
             isValid,
-            touched,
+            // touched,
             handleChange,
             handleBlur,
-            handleSubmit,
-            isSubmitting,
+            // handleSubmit,
+            // isSubmitting,
             // https://github.com/jaredpalmer/formik/issues/114
             // touched 必須在 blur 時，才會判斷已 touch，但會有一開始 onInput 時就 trigger 的情況
             // 所以在 onChange 時就設定已 touch，讓 validation error message 能正常顯示
-            // setFieldTouched('fieldName', isTouched, shouldValidate)
             setFieldTouched,
-            /* and other goodies */
+            setFieldValue,
         } = this.props;
-
-        console.log('error', errors);
-        console.log('touch', touched);
         return (
-        <div className="LoginForm w-80p h-80vh p-lg pos-ab-center bg-white">
-            <form className="d-b">
-                <div className="LoginForm-title">
-                    歡迎註冊
-                </div>
+            <div className="LoginForm w-80p h-80vh p-lg pos-ab-center bg-white">
+                <form className="d-b" onSubmit={e => { e.preventDefault() }}>
+                    <div className="LoginForm-title">
+                        歡迎註冊
+                    </div>
 
-                {/* 手機 */}
-                <FormControl label="您的手機號碼" errorMessage={this.getErrorMessage('phone')}>
-                    <div className="form-control-combine">
-                        <Select
-                            name="country"
-                            value={values.country}
-                            onChange={handleChange}
-                            wrapClass="form-control-combine-select w-40p d-i-b vertical-align-top"
-                            options={this.countryOptions}
-                        />
+                    {/* 手機 */}
+                    <FormControl label="您的手機號碼" errorMessage={this.getErrorMessage('phone')}>
+                        <div className="form-control-combine">
+                            <Select
+                                name="country"
+                                value={values.country}
+                                onChange={handleChange}
+                                wrapClass="form-control-combine-select w-40p d-i-b vertical-align-top"
+                                options={this.countryOptions}
+                            />
+                            <Input
+                                name="phone"
+                                type="text"
+                                value={values.phone}
+                                onChange={this.handleFormikInputChange({
+                                    setFieldTouched,
+                                    handleChange
+                                })}
+                                onBlur={handleBlur}
+                                wrapClass="form-control-combine-input w-60p  d-i-b vertical-align-top"/>
+                        </div>
+                    </FormControl>
+
+                    {/* 地址 */}
+                    <FormControl label="地址" errorMessage={this.getErrorMessage('addressDetail')}>
+                        <div className="form-control-combine">
+                            <Select
+                                name="addressArea"
+                                value={values.addressArea}
+                                onChange={handleChange}
+                                wrapClass="form-control-combine-select w-40p d-i-b vertical-align-top"
+                                options={this.addressOptions}
+                            />
+                            <Input
+                                name="addressDetail"
+                                type="text"
+                                value={values.addressDetail}
+                                onChange={this.handleFormikInputChange({
+                                    setFieldTouched,
+                                    handleChange
+                                })}
+                                onBlur={handleBlur}
+                                wrapClass="form-control-combine-input w-60p  d-i-b vertical-align-top"/>
+                        </div>
+                    </FormControl>
+
+
+                    {/* 姓名 */}
+                    <FormControl label="會員姓名" errorMessage={this.getErrorMessage('name')}>
                         <Input
-                            name="phone"
+                            name="name"
                             type="text"
-                            value={values.phone}
+                            value={values.name}
                             onChange={this.handleFormikInputChange({
                                 setFieldTouched,
                                 handleChange
                             })}
                             onBlur={handleBlur}
-                            wrapClass="form-control-combine-input w-60p  d-i-b vertical-align-top"/>
+                            placeholder="請輸入真實中文姓名"
+                        />
+                    </FormControl>
+
+                    {/* 密碼 */}
+
+                    <FormControl label="密碼" errorMessage={this.getErrorMessage('password')}>
+                        <Input
+                            name="password"
+                            type="password"
+                            value={values.password}
+                            onChange={this.handleFormikInputChange({
+                                setFieldTouched,
+                                handleChange
+                            })}
+                            onBlur={handleBlur}
+                            placeholder="請輸入密碼"
+                        />
+                    </FormControl>
+                    {/* 確認密碼 */}
+
+                    <FormControl label="確認密碼" errorMessage={this.getErrorMessage('confirmPassword')}>
+                        <Input
+                            name="confirmPassword"
+                            type="password"
+                            value={values.confirmPassword}
+                            onChange={this.handleFormikInputChange({
+                                setFieldTouched,
+                                handleChange
+                            })}
+                            onBlur={handleBlur}
+                            placeholder="請輸入密碼"
+                        />
+                    </FormControl>
+                    {/* Email */}
+
+                    <FormControl label="Email" errorMessage={this.getErrorMessage('email')}>
+                        <Input
+                            name="email"
+                            type="email"
+                            value={values.email}
+                            onChange={this.handleFormikInputChange({
+                                setFieldTouched,
+                                handleChange
+                            })}
+                            onBlur={handleBlur}
+                            placeholder="請輸入 Email"
+                        />
+                    </FormControl>
+                    {/* 手機驗證碼 */}
+                    <FormControl label="手機驗證碼" errorMessage={this.getErrorMessage('otp')}>
+                        <Input
+                            name="otp"
+                            type="text"
+                            value={values.otp}
+                            onChange={this.handleFormikInputChange({
+                                setFieldTouched,
+                                handleChange
+                            })}
+                            onBlur={handleBlur}
+                            placeholder="請輸入驗證碼"
+                            style={{
+                                paddingRight: '32%',
+                            }}
+                            appendElementRender={() => {
+                                return (
+                                    <button
+                                        className="LoginForm-phone-btn btn d-i-b w-30p pos-ab t-50p r-5 translate-y-50p"
+                                        onClick={e => {
+                                            setFieldValue('otp', '', true);
+                                        }}
+                                    >
+                                        取得手機驗證碼
+                                    </button>
+                                );
+                            }}
+                        />
+                    </FormControl>
+                    
+                    {/* Agree */}
+                    <div className="fz-sm m-t-sm">
+                        <label>
+                            <input
+                                name="agree"
+                                type="checkbox"
+                                className="d-i-b vertical-align-middle"
+                                value={values.agree}
+                                onChange={handleChange}
+                            />
+                            <span>我同意個人資料保護法</span>
+                            <a
+                                href="https://www.google.com"
+                                title="行銷資訊發送"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                &lt;規定&gt;
+                            </a>
+                        </label>
                     </div>
-                </FormControl>
-
-                {/* 姓名 */}
-                <FormControl label="會員姓名" errorMessage={this.getErrorMessage('name')}>
-                    <Input
-                        name="name"
-                        type="text"
-                        value={values.name}
-                        onChange={this.handleFormikInputChange({
-                            setFieldTouched,
-                            handleChange
-                        })}
-                        onBlur={handleBlur}
-                        placeholder="請輸入真實中文姓名"
-                    />
-                </FormControl>
-
-                {/* 密碼 */}
-
-                <FormControl label="密碼" errorMessage={this.getErrorMessage('password')}>
-                    <Input
-                        name="password"
-                        type="password"
-                        value={values.password}
-                        onChange={this.handleFormikInputChange({
-                            setFieldTouched,
-                            handleChange
-                        })}
-                        onBlur={handleBlur}
-                        placeholder="請輸入密碼"
-                    />
-                </FormControl>
-                {/* 確認密碼 */}
-
-                <FormControl label="確認密碼" errorMessage={this.getErrorMessage('confirmPassword')}>
-                    <Input
-                        name="confirmPassword"
-                        type="password"
-                        value={values.confirmPassword}
-                        onChange={this.handleFormikInputChange({
-                            setFieldTouched,
-                            handleChange
-                        })}
-                        onBlur={handleBlur}
-                        placeholder="請輸入密碼"
-                    />
-                </FormControl>
-                {/* Email */}
-
-                <FormControl label="Email" errorMessage={this.getErrorMessage('email')}>
-                    <Input
-                        name="email"
-                        type="email"
-                        value={values.email}
-                        onChange={this.handleFormikInputChange({
-                            setFieldTouched,
-                            handleChange
-                        })}
-                        onBlur={handleBlur}
-                        placeholder="請輸入 Email"
-                    />
-                </FormControl>
-                {/* 手機驗證碼 */}
-                <FormControl label="手機驗證碼" errorMessage={this.getErrorMessage('otp')}>
-                    <Input
-                        name="otp"
-                        type="number"
-                        value={values.otp}
-                        onChange={this.handleFormikInputChange({
-                            setFieldTouched,
-                            handleChange
-                        })}
-                        onBlur={handleBlur}
-                        placeholder="請輸入驗證碼"
-                        style={{
-                            paddingRight: '32%',
-                        }}
-                        appendElementRender={() => {
-                            return (
-                                <button
-                                    className="LoginForm-phone-btn btn d-i-b w-30p pos-ab t-50p r-5 translate-y-50p"
-                                >
-                                    取得手機驗證碼
-                                </button>
-                            );
-                        }}
-                    />
-                </FormControl>
-                {/* Submit */}
-                <button
-                    className="LoginForm-submit btn btn-submit m-b-sm btn-border"
-                    disabled={!isValid}
-                >
-                    確定註冊
-                </button>
-                {/* Agree */}
-                <div className="fz-sm">
-                    <label>
-                        <input type="checkbox" className="d-i-b vertical-align-middle"/>
-                        <span>我願意接收最新優惠通知</span>
-                        <a className="n-blue--link" href="/HelpCenter/MarketInfo" title="行銷資訊發送" target="_blank">&lt;行銷資訊發送&gt;</a>
-                    </label>
-                </div>
-                
-            </form>
-        </div>
+                    {/* Submit */}
+                    <button
+                        className="LoginForm-submit btn btn-submit m-t-sm btn-border"
+                        disabled={!isValid}
+                    >
+                        確定註冊
+                    </button>
+                </form>
+            </div>
         )
     }
 }
