a4e1e86a05894d69b86b9f1005e95bfd4a9e87e2
jason <jason.huang@ehsn.coim.tw>
2019-07-30 17:04:34 +0800

    update validate
    - add addressArea & addressDetail & agree rules 
    - updat otp rules
    -  remove handleSubmit function

--- src/hoc/validateRegisterForm.js
+++ src/hoc/validateRegisterForm.js
@@ -8,90 +8,86 @@ export default withFormik({
         return { 
             country: '',
             phone: '',
+            addressArea: '',
+            addressDetail: '',
             name: '',
             password: '',
             confirmPassword: '',
             email: '',
             otp: '',
+            agree: false,
         };
-    },
-    initialValues: { 
-        country: '',
-        phone: '',
-        name: '',
-        password: '',
-        confirmPassword: '',
-        email: '',
-        otp: '',
     },
     // https://cythilya.github.io/2019/03/12/formik-and-yup/
     // validate & validationSchema 只能則ㄧ存在
-    // validate: values => {
-    //     console.log('validate', values)
-    //     let errors = {};
-    //     if (!values.email) {
-    //         errors.email = 'Email Required';
-    //     } else if (
-    //         !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-    //     ) {
-    //         errors.email = 'Invalid email address';
-    //     }
-
-    //     if (!values.customPassword) {
-    //         errors.customPassword = 'customPassword Required';
-    //     }
-    //     return errors;
-    // },
 
     validationSchema: props => {
         // https://github.com/jaredpalmer/formik/issues/145
         return validatePassword.lazy(values => {
-            console.log('val', values)
-            // let str = '';
-            // switch(values.country){
-            //     case "886":
-            //         str = '台灣電話要必填'
-            //         break;
-            //     case "852":
-            //         str  = '香港電話要必填'
-            //         break;
-            //     default:
-            //         str = '這是必填欄位';
-            //         break;
+            let phoneReg = /((?=(09))[0-9]{10})$/g;
+            switch(values.country){
+                case "886":
+                    phoneReg = /((?=(09))[0-9]{10})$/g;
+                    break;
+                case "86":
+                    phoneReg  = /^(?=\d{11}$)^1(?:(?:3(?!49)[4-9\D]|47|5[012789]|78|8[23478]|98)\d{8}$|(?:70[356])\d{7}$)/g;
+                    break;
+                default:
+                    phoneReg = /((?=(09))[0-9]{10})$/g;
+                    break;
 
-            // }
+            }
             return validatePassword.object().shape({
+                country: validatePassword
+                    .string()
+                    .notRequired(),
                 phone: validatePassword
                     .string()
-                    .required('這是必填欄位!'),
+                    .required('這是必填欄位!')
+                    .matches(phoneReg, '電話格式有誤!'),
                 name:  validatePassword
                     .string()
                     .required('這是必填欄位!'),
+                addressArea: validatePassword
+                    .string()
+                    .notRequired(),
+                addressDetail: validatePassword
+                    .string()
+                    .when('addressArea', (addressArea, yup) => {
+                        if (addressArea && addressArea !== '') {
+                            return yup.required('請輸入地址!');
+                        }
+                        return yup;
+                    }),
                 password: validatePassword
                     .string()
+                    .min(6, '最少 6 位數')
+                    .max(8, '最多 8 位數')
                     .required('這是必填欄位!'),
                 confirmPassword: validatePassword
                     .string()
-                    .required('這是必填欄位!'),
+                    .required('這是必填欄位!')
+                    .oneOf([values.password], '密碼需一致!'),
                     // .string().customPassword(`The password format is wrong.`),
                 email: validatePassword
                     .string()
                     .required('這是必填欄位!')
-                    .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, `It's illegal Email format the word @ must between Number and English.`),
+                    .email(`Email 格式有誤!!`),
+                    // .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, `Email 格式有誤!!`),
                 otp: validatePassword
-                    .string()
+                    .number()
+                    // https://github.com/jquense/yup/issues/47#issuecomment-215588412
+                    .typeError('請輸入 0-9 正整數 !')
+                    .integer('請輸入 0-9 正整數 !')
                     .required('這是必填欄位!'),
+                agree: validatePassword
+                    .boolean()
+                    .oneOf([true], '請勾選我同意!'),
             }) 
         })
     },
-    handleSubmit: (values, { setSubmitting }) => {
-        setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-        }, 400);
-    },
     // 若要 submit 時在 validate 則 onChange onBlur 驗證 option 要關掉
     // validateOnChange: false,
-    validateOnBlur: false,
+    // validateOnBlur: false,
     displayName: 'ValidateRegisterForm',
 })
