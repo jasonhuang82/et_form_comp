392037def205a4f9158e064df52fdd19ba4f8b19
jason <jason.huang@ehsn.coim.tw>
2019-07-30 16:52:40 +0800

    add react animate

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,9 +3,10 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
+  Redirect,
 } from "react-router-dom";
 import './App.scss';
-
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 // pages
 import LoginForm from '../../pages/LoginForm/loginForm';
 import PurchaseForm from '../../pages/PurchaseForm/purchaseForm';
@@ -16,16 +17,28 @@ import Header from '../../components/Header/header';
 const App = () => {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        <Header />
-        <main className="App_content">
-          <Switch>
-            <Route exact path="/" component={LoginForm} />
-            <Route path="/purchase" component={PurchaseForm} />
-            <Route path="*" render={props => <div>404</div>}/>
-          </Switch>
-        </main>
-      </div>
+      <Route
+        render={({ location }) => (
+          <div className="App">
+            <Header />
+            <main className="App_content">
+              <TransitionGroup>
+                    <CSSTransition
+                      key={location.key}
+                      classNames="App-transform-page"
+                      timeout={300}
+                    >
+                      <Switch location={location}>
+                        <Route exact path="/" component={LoginForm} />
+                        <Route path="/purchase" component={PurchaseForm} />
+                        <Route path="*" render={props => <Redirect to="/"/>}/>
+                      </Switch>
+                    </CSSTransition>
+                  </TransitionGroup>
+            </main>
+          </div>
+        )}
+      />
     </Router>
   );
 }
